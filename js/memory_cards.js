75c62cb9e0c4c8c11bf0d952487dbe83a3d843f6
marcoruz <marco.ruzzo06@googlemail.com>
2023-07-18 23:25:24 +0200

    memory_cards code am 18.07.23

---
+++ js/memory_cards.js
@@ -0,0 +1,29 @@
+//_______________________________________________________________________________________
+// Hier werden die Karten abgelegt
+
+export default {
+
+    // Standartordnerpfad der Bilder
+    dir: '/img/fruits_picture/',
+  
+    // Anzahl der Bilder
+    amount: 16,
+
+    // Funktion zum Aktualisieren des Ordnerpfads
+    updateDir(newDir) {
+        this.dir = newDir;
+      },
+      
+      newDir: '/img/Hardware_picture/',
+
+    getCards() {
+        let cards = [];
+        for (let i=1; i<=this.amount; i++) {
+            cards.push({
+                id: i,
+                img: `${this.dir}${i < 10 ? '0' : ''}${i}.jpg`
+            });
+        }
+        return cards;
+    }
+  }
