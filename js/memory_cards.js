471fe53955d06f459da4ebd9353c052ad611a573
marcoruz <marco.ruzzo06@googlemail.com>
2023-07-19 12:44:47 +0200

    Für js ein Ordner

--- js/memory_cards.js
+++ js/memory_cards.js
@@ -3,27 +3,27 @@
 
 export default {
 
-    // Standartordnerpfad der Bilder
-    dir: '/img/fruits_picture/',
-  
-    // Anzahl der Bilder
-    amount: 16,
+  // Standartordnerpfad der Bilder
+  dir: '/img/fruits_picture/',
 
-    // Funktion zum Aktualisieren des Ordnerpfads
-    updateDir(newDir) {
-        this.dir = newDir;
-      },
-      
-      newDir: '/img/Hardware_picture/',
+  // Anzahl der Bilder
+  amount: 16,
 
-    getCards() {
-        let cards = [];
-        for (let i=1; i<=this.amount; i++) {
-            cards.push({
-                id: i,
-                img: `${this.dir}${i < 10 ? '0' : ''}${i}.jpg`
-            });
-        }
-        return cards;
-    }
+  // Funktion zum Aktualisieren des Ordnerpfads
+  updateDir(newDir) {
+      this.dir = newDir;
+    },
+    
+    newDir: '/img/Hardware_picture/',
+
+  getCards() {
+      let cards = [];
+      for (let i=1; i<=this.amount; i++) {
+          cards.push({
+              id: i,
+              img: `${this.dir}${i < 10 ? '0' : ''}${i}.jpg`
+          });
+      }
+      return cards;
   }
+}
